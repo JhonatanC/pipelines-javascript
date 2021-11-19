8bb4bcbfa246129fe8d0c438c0e1b271acb09c94
Jhonatan C Hurtado <jhonatan.cortes@servinformacion.com>
2021-11-19 15:15:12 -0500

    nuevos cambios

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Cambiando el puesto para que no choque con otros en uso');
+  res.send('nuevos cambios');
 });
 
 var port = process.env.PORT||PORT;
